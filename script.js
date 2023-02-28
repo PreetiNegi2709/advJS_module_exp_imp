596e0f2b7dee9c4de0d99050e29c840cad6f238d
PreetiNegi2709 <105388761+PreetiNegi2709@users.noreply.github.com>
2023-02-28 14:11:04 +0530

    alias in import

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 
 import n from './name.js';
 import {sAge} from './stuDetail.js';
-import {class_s} from './stuDetail.js';
+// import {class_s} from './stuDetail.js';
+import { class_s as stnd } from './stuDetail.js';
 
+// console.log(stnd)
 btn.addEventListener("click",function(){
     let stuName = document.querySelector(".nameC1")
     stuName.innerHTML= `${n.stuName}`
     let stuAge = document.querySelector(".ageC")
     stuAge.innerHTML= `${sAge.age}`
     let stuClass = document.querySelector(".classC")
-    stuClass.innerHTML= `${class_s}`
+    stuClass.innerHTML= `${stnd}`
 })
